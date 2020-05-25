d8c6c2a98105bc0be0738a5f73e012fc61b70dc9
Andrade Guzman <ravens452@gmail.com>
2020-05-24 22:56:16 -0500

    Initial Commit

---
+++ Laboratorio_4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Laboratorio_4
+//
+//  Created by carlos andrade guzman on 5/24/20.
+//  Copyright © 2020 carlos andrade guzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
